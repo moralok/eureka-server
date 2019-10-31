d44938caa5f0a57b16833f3eb188d37919f248c8
moralok <moralok@163.com>
2019-10-31 11:29:51 +0800

    init

---
+++ src/test/java/com/moralok/trial/TrialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moralok.trial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
